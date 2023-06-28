9e033a77dc7a758678313211b51d2fbbee5c6ec9
Daniel Lin <lin00133@umn.edu>
2023-06-28 14:53:22 -0700

    Restored config file

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-dsvavewavwverversger
+#discord bot token
+TOKEN = '<your token>'
+
+# login credentials for swissonlinetournament.com
+username = '<username>'
+password = '<password>'
+# code that comes after 'https://swissonlinetournament.com/Tournament/Details/'
+tournament = '<rest of url>'
