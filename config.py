33ab58007f2f4c1af2500abb60fe7dabc4f68dc7
Daniel Lin <lin00133@umn.edu>
2023-06-28 14:46:59 -0700

    test

--- config.py
+++ config.py
@@ -1,8 +1,1 @@
-#discord bot token
-TOKEN = '<your token>'
-
-# login credentials for swissonlinetournament.com
-username = '<username>'
-password = '<password>'
-# code that comes after 'https://swissonlinetournament.com/Tournament/Details/'
-tournament = '<rest of url>'
+dsvavewavwverversger
